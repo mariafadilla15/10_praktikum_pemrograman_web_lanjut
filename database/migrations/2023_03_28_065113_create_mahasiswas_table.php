84c19ed5c3afdea4c5a8ffd0c0693a438b39b2ce
= mariafadilla15 <= mariafadilla15@gmail.com>
2023-03-29 00:08:35 +0700

    JS7

---
+++ database/migrations/2023_03_28_065113_create_mahasiswas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->integer('Nim')->primary();
+            $table->string('Nama', 100)->nullable();
+            $table->string('Kelas', 50)->nullable();
+            $table->string('Jurusan', 100)->nullable();
+            $table->string('No_Handphone', 100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
